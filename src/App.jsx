4897797924de73790d57135922ab6ae4fb3c1d06
Luis <luis-corbi@outlook.com>
2021-11-30 11:56:07 -0300

    desafío clase 3

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <h1>
+          Mobilestore
+        </h1>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+export default App;
