459a2a7da503bab2c6c61b83c5dd6d2714f19690
Luis <luis-corbi@outlook.com>
2021-12-30 20:33:39 -0300

    desafío clase 12

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbMLNvRPWQbJlOHcL1fxjWvuKvkfRM_VU",
+  authDomain: "mobilestore-a01f5.firebaseapp.com",
+  projectId: "mobilestore-a01f5",
+  storageBucket: "mobilestore-a01f5.appspot.com",
+  messagingSenderId: "567079825981",
+  appId: "1:567079825981:web:40e7068ba9d647488e3054",
+  measurementId: "G-ECY6SDLQ11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
