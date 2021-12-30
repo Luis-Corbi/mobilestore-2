997c324f44e9499774cd9a0a9a0e8472b9339baa
Luis <luis-corbi@outlook.com>
2021-12-30 20:59:36 -0300

    desafío clase 12

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,3 +1,6 @@
+import { collection, getDocs, getFiresore, getFirestore } from "firebase/firestore";
+import { useEffect } from "react";
+
 const Items = [
     {id: 1, name: "IPhone 13 Pro", price: 1299.99, foto: "https://http2.mlstatic.com/D_NQ_NP_840175-MLA47779316482_102021-O.webp", categoria: "celulares"},
     {id: 2, name: "IPhone 12 Pro", price: 1099.99, foto: "https://http2.mlstatic.com/D_NQ_NP_824876-MLA43975720984_112020-O.webp", categoria: "celulares"},
@@ -5,14 +8,14 @@ const Items = [
     {id: 4, name: "Xiaomi Mi 11 Ultra", price: 920, foto: "https://http2.mlstatic.com/D_NQ_NP_751583-MLA46773535610_072021-O.webp", categoria: "celulares"}
 ];
 
-const getproducts = () => {
-    return new Promise((resolve, reject) => {
-       setTimeout(() => {
-        resolve(Items);
-       }, 1500);
-});
-}
+useEffect(() => {
+    const db = getFirestore();
 
-getproducts().then((Items) => console.log(Items));
+    const itemCollection = collection(db, "items");
+    getDocs(itemCollection).then((snapshot) => {
+        setProducts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+    });
+}; []);
 
-export default getproducts
+
+export default itemCollection
